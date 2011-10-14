39c96f874ee8f9ad0d81e3ef957391955817c742
Sebastian <sebastian.lipp@gmx.net>
2011-10-15 01:58:31 +0200

    NetFinder Version 0.01 Build 111015

---
+++ netcon Software/NetFinder/src/NetFinder.java
@@ -0,0 +1,83 @@
+/*
+	NetFinder Version 0.01 Build 111015
+
+*/
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class NetFinder {
+	
+	public static void main(String[] args) {
+		
+		try {
+			System.out.println("netcon netfinder an " +  InetAddress.getLocalHost().toString() + "\n");
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		while(true) {
+			
+			System.out.println("Press RETURN to find modules in the network");
+			
+			InputStreamReader isr = new InputStreamReader(System.in);
+			BufferedReader br = new BufferedReader(isr);
+			
+			try{
+				br.readLine();
+			} catch(Exception e) {
+				e.printStackTrace();
+				}
+			
+			try{
+				Network.sendBroadcast("bcnetcon", 6799);
+			} catch (IOException e) {
+				e.printStackTrace();
+				}
+			
+			DatagramPacket recv = null;
+			String message = null;
+			
+			System.out.println("Search started - found modules: ");
+			
+			long startTime = System.currentTimeMillis();
+			
+			boolean found = false;
+			
+			while(true) {
+				
+				if(System.currentTimeMillis() - startTime > 15000)
+					break;
+				
+					try {
+						recv = Network.receivePacket(6800);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					if( recv != null) {
+						found = true;
+						message = new String(recv.getData());
+						System.out.println(message + " (" + recv.getAddress().toString() + ":" + recv.getPort() + ")");
+					}
+					
+			}
+			
+			System.out.println("Search complete");
+			
+			if(found == false) 
+				System.out.print("...No modules found!");
+			
+			System.out.println("\n");
+					
+		}
+	}		
+}
---
+++ netcon Software/NetFinder/src/Network.java
@@ -0,0 +1,77 @@
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketException;
+
+
+public class Network {
+	
+	private static String group = "255.255.255.255";
+	
+	public static void sendBroadcast(String msg, int dstPort) throws IOException {
+		
+		MulticastSocket socket = new MulticastSocket();
+		
+		DatagramPacket msgPacket = new DatagramPacket(msg.getBytes(), msg.length(), InetAddress.getByName(group), dstPort);
+		
+		socket.send(msgPacket);
+		
+		socket.close();
+	}
+			
+	
+	public static DatagramPacket receiveBroadcast(int lstPort) throws IOException {
+		
+		MulticastSocket socket = new MulticastSocket(lstPort);
+	
+		// receive broadcast
+		byte[] buf = new byte[100];
+		
+		DatagramPacket recv = new DatagramPacket(buf, buf.length);
+		socket.receive(recv);
+	
+		socket.close();
+		
+		return recv;		
+		
+	}
+	
+	public static void sendPacket(String msg, InetAddress dstAdr, int dstPort) throws IOException {
+		
+		DatagramSocket socket = new DatagramSocket();
+		
+		DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.length(), dstAdr, dstPort);
+		
+		socket.send(packet);
+		
+		socket.close();
+		
+	}
+	
+	public static DatagramPacket receivePacket(int lstPort) throws SocketException {
+		
+		DatagramSocket socket = new DatagramSocket(lstPort);
+		
+		byte[] buf = new byte[100];
+		
+		DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
+		socket.setSoTimeout(5000);
+		
+		try {
+			socket.receive(packet);
+		} catch (IOException e) {
+			packet = null;
+			socket.close();
+			return packet;
+		}
+		
+		socket.close();
+		
+		return packet;
+	}
+
+}
