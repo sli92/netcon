3b72219a361ea9ab98ad65d2d0f92ec8b6b220a7
Sebastian <sebastian.lipp@gmx.net>
2011-10-26 22:06:05 +0200

    NetFinder(Bundle) Version 0.02
    NetFinder Version 0.01 Build 111026

--- netcon Software/NetFinder/src/program/NetFinder.java
+++ netcon Software/NetFinder/src/program/NetFinder.java
@@ -1,80 +1,78 @@
 package program;
+
 /*
-	NetFinder Version 0.01 Build 111020
+ *  NetFinder(Bundle) Version 0.02
+ *  NetFinder Version 0.02 Build 111026
+ */
 
-*/
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import lib.Module;
+import lib.ModuleStock;
+import lib.NetFFrame;
 import lib.Network;
 
 public class NetFinder {
 	
 	public static void main(String[] args) {
+
+		ModuleStock list = new ModuleStock();
 		
 		try {
-			System.out.println("netcon netfinder an " +  InetAddress.getLocalHost().toString() + "\n");
-		} catch (UnknownHostException e1) {
-			e1.printStackTrace();
-		}
-		
-		while(true) {
-			
-			System.out.println("Press RETURN to find modules in the network");
-			
-			InputStreamReader isr = new InputStreamReader(System.in);
-			BufferedReader br = new BufferedReader(isr);
-			
-			try{
-				br.readLine();
-			} catch(Exception e) {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (Exception e) {
 				e.printStackTrace();
-				}
-			
-			try{
-				Network.sendPacket("bcnetcon", InetAddress.getByName("255.255.255.255"), 6799, 6800);
+			}
+
+		JFrame frame = new NetFFrame(list);
+		frame.setVisible(true);
+
+		List<Module> tempList = null;
+
+		while (true) {
+
+			try {
+				Network.sendPacket("bcnetcon",
+						InetAddress.getByName("255.255.255.255"), 6799, 6800);
 			} catch (IOException e) {
 				e.printStackTrace();
-				}
-			
-			DatagramPacket recv = null;
-			String message = null;
-			
-			System.out.println("Search started - found modules: ");
-			
-			boolean found = false;
-			
-			while(true) {
-								
-					try {
-						recv = Network.receivePacket(6800, 4000);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-					
-					if( recv != null) {
-						found = true;
-						message = new String(recv.getData());
-						message = message.trim();
-						System.out.println(message + " (" + recv.getAddress().toString() + ":" + recv.getPort() + ")");
-					}
-					else
-						break;
-					
 			}
-			
-			System.out.println("Search complete");
-			
-			if(found == false) 
-				System.out.print("...No modules found!");
-			
-			System.out.println("\n");
-					
+
+			DatagramPacket recv = null;
+			Module module = null;
+			tempList = new ArrayList<Module>();
+
+			while (true) {
+
+				module = null;
+
+				try {
+					recv = Network.receivePacket(6800, 2000);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				if (recv != null) {
+					module = new Module(new String(recv.getData()).toString(),
+							recv.getAddress(), recv.getPort());
+					tempList.add(module);
+
+				} else
+					break;
+
+			}
+
+			list.equalize(tempList);
+
 		}
-	}		
+
+	}
+
 }
