e74b22851d5593aae38d60a3ea9883df03164b38
dev00 <e_snake_w@hotmail.com>
2012-03-02 00:04:36 +0100

    Alternative neton web Oberfläche gebastelt, vergib mir :D

---
+++ netcon Software/netcon web alternativ/connector.php
@@ -0,0 +1,45 @@
+<?php
+header('Content-Type: text/html; charset=utf-8'); // sorgt für die korrekte Kodierung
+header('Cache-Control: must-revalidate, pre-check=0, no-store, no-cache, max-age=0, post-check=0'); // ist mal wieder wichtig wegen IE
+
+
+            $out = "";
+			$host="localhost";
+			$port=5004;
+			$timeout=30;
+			
+			
+			$sk=fsockopen($host,$port,$errnum,$errstr,$timeout);
+			
+			if(!is_resource($sk)) {
+				
+				exit("error");
+		
+			}
+			else {
+					
+					fputs($sk, "GET\n", 4);
+					fputs($sk, "list\n", 5);
+					
+					$zeichen = fgetc($sk);
+						
+						while($zeichen != "\n") {
+
+							$out .= $zeichen;
+							$zeichen = fgetc($sk);
+						}
+						
+								
+					echo $out;
+					
+					
+
+					
+				
+			}
+			
+			fclose($sk);	
+
+
+?>
+
---
+++ netcon Software/netcon web alternativ/netcon.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+    window.setInterval(refreshContent, 1000);
+    refreshContent();
+});
+
+function typeToString(typeNum) {
+    switch (typeNum) {
+        case 1:
+            return "Spannung";
+            break;
+    }
+}
+
+function dtypeToString(typeChar) {
+    switch (typeChar) {
+        case 'f':
+            return "Float";
+            break;
+    }
+}
+
+function shortUnitForType(typeNum) {
+    switch (typeNum) {
+        case 1:
+            return 'V';
+            break;
+    }
+}
+
+// http://codeaid.net/javascript/convert-seconds-to-hours-minutes-and-seconds-%28javascript%29
+function rectime(seconds) {
+    var numdays = Math.floor(seconds / 86400);
+    var numhours = Math.floor((seconds % 86400) / 3600);
+    var numminutes = Math.floor(((seconds % 86400) % 3600) / 60);
+    var numseconds = ((seconds % 86400) % 3600) % 60;
+
+    return numdays + "d " + numhours + ":" + numminutes + ":" + numseconds;
+
+}
+
+function refreshContent() {
+    $.get('connector.php', function(result){
+        if(result.indexOf("error") >= 0) {
+            var tableHTML = '';
+            tableHTML += '<tr><th>Name</th><th>Standort</th><th>Uptime</th><th>Devices</th></tr>';
+            var tab = $("#modulelist");
+            tab.html(tableHTML);
+            return;
+        }
+        var json = $.parseJSON(result);
+        
+        var tableHTML = '';
+        // Generate Header
+        tableHTML += '<tr><th>Name</th><th>Standort</th><th>Uptime</th><th>Devices</th></tr>';
+        
+        
+        for(var i = 0; i < json.modulliste.length; i++) {
+            var modul = json.modulliste[i];
+            
+            tableHTML += '<tr><td style="width: 180px; text-align: center;">' + modul.name + '</td><td style="width: 120px; text-align: center;">' + modul.location + '</td><td style="width: 90px; text-align: center;">' + rectime(parseInt(modul.uptime / 100)) + '</td>';
+            
+            tableHTML += '<td><table><tr><th>ID</th><th>Typ</th><th>Datentyp</th><th>Min</th><th>Max</th><th>Aktuell</th><th></th></tr>';
+            for(var z = 0; z < modul.devicelist.length; z++) {
+                var device = modul.devicelist[z];
+                var unit = shortUnitForType(device.type);
+                var proz = parseInt(device.value / (device.max - device.min) * 100);
+                
+                tableHTML += '<tr><td style="width: 30px; text-align: right;">' + device.id + '</td><td style="width: 90px; text-align: center;">' + typeToString(device.type) + '</td><td style="width: 80px; text-align: center;">' + dtypeToString(device.dtype) + '</td><td style="width: 60px; text-align: right;">' + device.min + unit + '</td><td style="width: 60px; text-align: right;">' + device.max + unit + '</td><td style="width: 60px; text-align: right;">' + device.value + unit + '</td><td style="width: 100px; border: 1px solid blue;"><div style="background-color: blue; border: 1px solid blue; width: ' + proz + 'px; height: 1em;"></div></td></tr>';
+            }
+            tableHTML += '</table></td>';
+        }
+        
+        
+        
+        var tab = $("#modulelist");
+        tab.html(tableHTML);
+    });
+}
