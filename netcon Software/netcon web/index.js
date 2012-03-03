1fb80c092d37aeeb907bf1b64892ae4335b3a07c
Sebastian <sebastian.lipp@gmx.net>
2012-03-03 11:23:44 +0100

    Milestone 2.031 (alles neu gemacht, jetzt zufrieden)
    Neuartiges WebUI auf DataTables-Basis. Jede Zeile ist nun ein Device
    und nicht mehr ein Modul.

--- netcon Software/netcon web/index.js
+++ netcon Software/netcon web/index.js
@@ -1,27 +1,21 @@
-$(document).ready(function(){
-	
-    window.setInterval(refreshContent, 1000);
-    refreshContent();
+$(document).ready(function(){ 
+		
+		initTable();
+		updateTable();
+		window.setInterval(updateTable, 1000);
+}); 
 
-});
+function changeTimeformat(seconds) {
+    var days = Math.floor(seconds / 86400);
+    var hours = Math.floor((seconds % 86400) / 3600);
+    var minutes = Math.floor(((seconds % 86400) % 3600) / 60);
+    var seconds = ((seconds % 86400) % 3600) % 60;
 
-function typeToString(typeNum) {
-    switch (typeNum) {
-        case 1:
-            return "Spannung";
-            break;
-    }
+    return days + "d " + hours + ":" + minutes + ":" + seconds;
+
 }
 
-function dtypeToString(typeChar) {
-    switch (typeChar) {
-        case 'f':
-            return "Float";
-            break;
-    }
-}
-
-function shortUnitForType(typeNum) {
+function getTypeunit(typeNum) {
     switch (typeNum) {
         case 1:
             return 'V';
@@ -29,82 +23,81 @@ function shortUnitForType(typeNum) {
     }
 }
 
-function rectime(seconds) {
-    var numdays = Math.floor(seconds / 86400);
-    var numhours = Math.floor((seconds % 86400) / 3600);
-    var numminutes = Math.floor(((seconds % 86400) % 3600) / 60);
-    var numseconds = ((seconds % 86400) % 3600) % 60;
+function initTable() {
+	
+	return $('#example').dataTable( {
 
-    return numdays + "d " + numhours + ":" + numminutes + ":" + numseconds;
-
+    		"bRetrieve": true,
+			"bJQueryUI": true,
+			"bProcessing": true,
+        	
+			"oLanguage": {
+                "sUrl": "./DataTables/media/dataTables.german.txt"
+            }
+		});   
 }
 
-function refreshContent() {
-    $.get('connector.php', function(result){
+function updateTable() {
+	
+	var oTable = initTable();
+	
+	$.get('connector.php', function(result){
+		
         if(result.indexOf("error") >= 0) {
+
+            if(result == "error") { 
+            	oTable.fnClearTable();
+            	oTable.fnAddData ( [
         	
-            var tableHTML = '';
-            if(result == "error") 
-            	tableHTML += 'Keine Verbindung';
-            else
-            	tableHTML += 'Keine Module gefunden';
-            var tab = $("#moduletable");
-            tab.html(tableHTML);
+        			"Keine Verbindung",
+        			"","","","","",""
+        			
+        			 ]
+        		);
+            }
+            else {
+            	oTable.fnClearTable();
+            }
+
             return;
         }
-        var json = $.parseJSON(result);
         
-        var tableHTML = '';
-
-        tableHTML += '<tr><th>Name</th><th>Standort</th><th>Uptime</th><th>Devices</th></tr></thead>';
+        oTable.fnClearTable();
         
+        var json = $.parseJSON(result);    
         
         for(var i = 0; i < json.modulliste.length; i++) {
-            var modul = json.modulliste[i];
-            
-
-            tableHTML += '<tr><td style="width: 180px; text-align: center;">'
-				+ modul.name
-				+ '</td><td style="width: 120px; text-align: center;">'
-				+ modul.location
-				+ '</td><td style="width: 90px; text-align: center;">'
-				+ rectime(parseInt(modul.uptime / 100))
-				+ '</td>';
-            
-            tableHTML += '<td><table><tr><th>ID</th><th>Typ</th><th>Datentyp</th><th>Min</th><th>Max</th><th>Aktuell</th><th></th></tr>';
-            for(var z = 0; z < modul.devicelist.length; z++) {
-                var device = modul.devicelist[z];
-                var unit = shortUnitForType(device.type);
-                var proz = parseInt(device.value / (device.max - device.min) * 100);
-                
-
-	            tableHTML += '<tr><td style="width: 30px; text-align: right;">'
-					+ device.id
-					+ '</td><td style="width: 90px; text-align: center;">'
-					+ typeToString(device.type)
-					+ '</td><td style="width: 80px; text-align: center;">'
-					+ dtypeToString(device.dtype)
-					+ '</td><td style="width: 60px; text-align: right;">'
-					+ device.min
-					+ unit
-					+ '</td><td style="width: 60px; text-align: right;">'
-					+ device.max
-					+ unit
-					+ '</td><td style="width: 60px; text-align: right;">'
-					+ device.value
-					+ unit
-					+ '</td><td style="width: 100px; border: 1px solid blue;"><div style="background-color: red; border: 1px solid blue; width: '
-					+ proz
-					+ 'px; height: 1em;"></div></td></tr>';
-	                
-            }
-            
-            tableHTML += '</table></td>';
-        }
-        
-        
-        
-        var tab = $("#moduletable");
-        tab.html(tableHTML);
+        	
+        	var modul = json.modulliste[i];
+        	
+        	for(var z = 0; z < modul.devicelist.length; z++) {
+        		
+        		var device = modul.devicelist[z];
+        		var percentage = parseInt(device.value / (device.max - device.min) * 100);
+        		var bar = '<table><td style="width: 100px; border: 1px solid red;padding: 0px 0px;">' 
+        					+ '<div style="background-color:red;width:' + percentage + 'px; height: 1em;"></div></td></table>';
+        		var unit = getTypeunit(device.type);
+        		
+        		oTable.fnAddData ( [
+        	
+        			modul.name,
+        			modul.location,
+        			changeTimeformat(parseInt(modul.uptime / 100)),
+        			"["+unit+"]",
+        			device.min,
+        			bar + device.value,
+        			device.max,
+        			
+        			 ]
+        		);
+        		
+        		
+        	}
+        	
+        	
+        }           
+   
     });
 }
+
+
